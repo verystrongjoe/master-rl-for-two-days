7bbfa6b28ac0caee95c2378e05ec8bb9ce58115f
UK, Jo <verystrongjoe@gmail.com>
2019-07-30 11:29:47 +0900

    Update 01.check-tf-2.0.py

--- 01.check-tf-2.0.py
+++ 01.check-tf-2.0.py
@@ -7,6 +7,8 @@ import tensorflow as tf
 import numpy as np
 import matplotlib.pyplot as plt
 
+
+# todo : 텐서플로우 2.0 에 대한 간단한 소개를 같이 여기에다가 ex) session에 대한 관리와 eagar execution에 대한 장점등..
 print(tf.__version__)
 print(tf.executing_eagerly())
 print("1 + 2 + 3 + 4 + 5 =", tf.reduce_sum([1, 2, 3, 4, 5]))
